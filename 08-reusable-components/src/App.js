7a2030a596ed9174c86cc064661c1cd0263e21df
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-28 18:00:34 +0300

    Created Text component

--- 08-reusable-components/src/App.js
+++ 08-reusable-components/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css'
 import MyComponent from "./components/MyComponent";
+import Text from "./components/Text";
 
 const App = () => {
     return (
         <div className="App">
             <MyComponent/>
+            <Text props='Props text'>
+                Children Text
+            </Text>
         </div>
     );
 };
