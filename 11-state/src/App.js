7a8b1f1a2b26cb4cd9eb4b0f00bf4a0c4296b0ae
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-30 15:13:45 +0300

    Created style

--- 11-state/src/App.js
+++ 11-state/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import RandomNumber from "./components/RandomNumber";
 
 const App = () => {
     return (
         <div className="App">
-
+            <RandomNumber/>
         </div>
     );
 };
---
+++ 11-state/src/components/RandomNumber.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RandomNumber = () => {
+    return (
+        <div>
+            <h1>1</h1>
+            <button>Generate new random number</button>
+        </div>
+    );
+};
+
+export default RandomNumber;
